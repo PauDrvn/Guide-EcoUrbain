6009e9bafff171d04ac741db2888a6132e4e8c85
Pauline Drvn <150014167+PauDrvn@users.noreply.github.com>
2024-06-23 17:02:09 +0200

    Add files via upload

--- JavaScript/vulga.js
+++ JavaScript/vulga.js
@@ -1,10 +1,11 @@
+// L'en tête ne s'affiche pas de suite
 document.addEventListener("DOMContentLoaded", function() {
   setTimeout(function() {
     document.querySelector(".image-transition").classList.add("loaded");
-  }, 1000); // Délai de 1 secondes avant que l'image ne soit progressivement affichée
+  }, 2000); // Délai de 2 secondes avant que l'image ne soit progressivement affichée
 });
 
-
+// Le chant d'oiseau est lu en passant sur son image
 var image = document.getElementById("chant-photo");
 var audio = document.getElementById("chant-oiseaux");
 
@@ -16,3 +17,4 @@ image.addEventListener("mouseleave", function() {
     audio.pause();
     audio.currentTime = 0; // Réinitialise la lecture à partir du début du son
 });
+
